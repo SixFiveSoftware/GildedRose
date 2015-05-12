a16ae0a4f6b8fa4e220712e49af57a81decefe90
BJ Miller <john.miller@dxydoes.com>
2015-05-12 09:21:01 -0400

    Initial Commit

---
+++ GildedRoseKata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GildedRoseKata
+//
+//  Created by Hank Turowski on 5/12/15.
+//  Copyright (c) 2015 Turowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
