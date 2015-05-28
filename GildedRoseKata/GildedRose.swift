b8ceac39504874cf333753ad5faaa1c994e8f6f8
Hank Turowski <hank.turowski@gmail.com>
2015-05-28 14:49:35 -0400

    Removed pesky semicolons

--- GildedRoseKata/GildedRose.swift
+++ GildedRoseKata/GildedRose.swift
@@ -64,7 +64,7 @@ class GildedRose {
                 {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros")
                     {
-                        items[i].quality = items[i].quality - 1;
+                        items[i].quality = items[i].quality - 1
                     }
                 }
             }
@@ -72,7 +72,7 @@ class GildedRose {
             {
                 if (items[i].quality < 50)
                 {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality = items[i].quality + 1
                     
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
                     {
@@ -80,14 +80,14 @@ class GildedRose {
                         {
                             if (items[i].quality < 50)
                             {
-                                items[i].quality = items[i].quality + 1;
+                                items[i].quality = items[i].quality + 1
                             }
                         }
                         if (items[i].sellIn < 6)
                         {
                             if (items[i].quality < 50)
                             {
-                                items[i].quality = items[i].quality + 1;
+                                items[i].quality = items[i].quality + 1
                             }
                         }
                     }
@@ -96,7 +96,7 @@ class GildedRose {
             
             if (items[i].name != "Sulfuras, Hand of Ragnaros")
             {
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn = items[i].sellIn - 1
             }
             
             if (items[i].sellIn < 0)
@@ -109,20 +109,20 @@ class GildedRose {
                         {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros")
                             {
-                                items[i].quality = items[i].quality - 1;
+                                items[i].quality = items[i].quality - 1
                             }
                         }
                     }
                     else
                     {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = items[i].quality - items[i].quality
                     }
                 }
                 else
                 {
                     if (items[i].quality < 50)
                     {
-                        items[i].quality = items[i].quality + 1;
+                        items[i].quality = items[i].quality + 1
                     }
                 }
             }
